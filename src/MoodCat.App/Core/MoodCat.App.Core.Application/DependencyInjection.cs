5d99308a05207f35efb6a96099b2c1932237818b
Cyprian Gburek <dcyprian.a.gburek@gmail.com>
2024-09-28 12:57:25 +0200

    Create DependencyInjection.cs

---
+++ src/MoodCat.App/Core/MoodCat.App.Core.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using MoodCat.App.Common.BuildingBlocks.Behaviors;
+
+namespace MoodCat.App.Core.Application;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Dodaje layer aplikacji z jej serwisami
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddApplicationLayerServices(this IServiceCollection services)
+    {                   
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+        
+        return services;
+    }
+}
